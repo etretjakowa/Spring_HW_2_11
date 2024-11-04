0474ffddebf4573c9d359e11409580ada3123ec1
etretjakova <etretjakowa@yandex.ru>
2024-11-04 19:00:52 +0300

    initial commit

---
+++ src/main/java/ru/sky/Spring/HW2/_1/SpringHw211Application.java
@@ -0,0 +1,13 @@
+package ru.sky.Spring.HW2._1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHw211Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sky/Spring/HW2/_1/SpringHw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sky.Spring.HW2._1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
